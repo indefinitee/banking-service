babec4c7a8da8e41dd5b5e674d30610f9f483104
indefinitee <danny.galaev@gmail.com>
2026-02-10 21:33:25 +0300

    add gin

---
+++ api/server.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/indefinitee/simplebank/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+// Старт HTTP сервера по адресу
+func (s *Server) Start(address string) error {
+	return s.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
