edc99c7f20557f88ef2c278796205ed87b27f1d1
indefinitee <danny.galaev@gmail.com>
2026-03-09 23:34:54 +0300

    add user request fields validation

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -9,12 +9,18 @@ import (
 	db "github.com/indefinitee/simplebank/db/sqlc"
 	"github.com/indefinitee/simplebank/pb"
 	"github.com/indefinitee/simplebank/util"
+	"github.com/indefinitee/simplebank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (s *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
 
 	user, err := s.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
@@ -75,4 +81,16 @@ func (s *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.L
 	}
 
 	return rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
 }
