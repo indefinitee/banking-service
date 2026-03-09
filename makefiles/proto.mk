012a57277045410d01f908973722cee94ac9bc4d
indefinitee <danny.galaev@gmail.com>
2026-03-09 22:47:31 +0300

    add swagger

--- makefiles/proto.mk
+++ makefiles/proto.mk
@@ -2,9 +2,11 @@
 
 clear_proto:
 	rm -f pb/*.go
+	rm -f doc/swagger/*.swagger.json
 
 proto: clear_proto
 	protoc --proto_path=proto --go_out=pb --go_opt=paths=source_relative \
     --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
 	--grpc-gateway_out=pb --grpc-gateway_opt=paths=source_relative \
+	--openapiv2_out=doc/swagger --openapiv2_opt=allow_merge=true,merge_file_name=simple_bank \
     proto/*.proto
